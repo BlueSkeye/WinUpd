2e84bc0338ad7ed69441535314238d2ce4b658f5
BlueSkeye <bsgithub@gmail.com>
2018-12-14 21:28:56 +0100

    Initial commit

---
+++ WinUpdExplorer/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinUpdExplorer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinUpdExplorer")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a5191475-3b8e-4aa7-85af-1175b37a02d5")]
+[assembly: AssemblyVersion("1.0.*")]
